c18974943ddd38dc25a13d0aa476b4a0c83b93c8
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:18:01 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 
+import random
+
 word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
 print(list0)
+
+randomWord=random.choice(word_list)
+print(randomWord)
