40f4bbd45bcbbf5151dc40f28f2f8ee5c3614baf
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:21:11 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -3,7 +3,9 @@
 import random
 
 word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
-print(list0)
+word_list=[x.lower() for x in word_list]
 
 word=random.choice(word_list)
-print(word)
+
+guess=input('Please enter a letter:\n')
+guess=guess.lower() # more robust if we are only dealing with lower case letters
