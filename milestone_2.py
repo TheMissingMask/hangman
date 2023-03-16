b811b13b0aea06041d304559d6a1d767040583ce
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:22:17 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -9,3 +9,8 @@ word=random.choice(word_list)
 
 guess=input('Please enter a letter:\n')
 guess=guess.lower() # more robust if we are only dealing with lower case letters
+
+if len(guess)!=1:
+  print('Please enter only a single letter')
+else:
+  print('You have guessed %s'%(guess))
