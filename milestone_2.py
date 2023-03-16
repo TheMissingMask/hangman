b20dffba90cc037a83114b2f9779fefba61de761
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:18:43 +0000

    Update milestone_2.py
    I hate these variable naming conventions!!!  Please let me code in the way I like to

--- milestone_2.py
+++ milestone_2.py
@@ -5,5 +5,5 @@ import random
 word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
 print(list0)
 
-randomWord=random.choice(word_list)
-print(randomWord)
+word=random.choice(word_list)
+print(word)
