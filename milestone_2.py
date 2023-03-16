e32027ebe56e1253b6f4cdcc60904a1cfcb31ba9
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:16:24 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 
-list0=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
+word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
 print(list0)
