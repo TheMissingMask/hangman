744f8be89405678534169d6fe2d6d6a84fb0b606
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-16 15:43:25 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -2,8 +2,9 @@
 
 import random
 
-word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
-word_list=[x.lower() for x in word_list]
+word_list=['rundown','tapout','underground','inside','carnival']
+#word_list=[x.lower() for x in word_list]
+#print(type(word_list))
 
 word=random.choice(word_list)
 
