86cdd4d710023f50bf60936fd54f11a321875079
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-17 16:55:34 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,7 @@
-#!/usr/bin/python
+import random
+
+word_list=['Rundown','Tap out','Maltese Falcon','Inside','Beantown Bailout']
+word_list=[x.lower() for x in word_list]
+word=random.choice(word_list)
+
 guess=input('Please enter a letter:\n')
