bb75329ac72e2af3bec55bd6bcc4e966216e44d0
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-17 20:50:53 +0000

    Update milestone_3.py
    if else to check if the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,8 @@
+import random
+
+word_list=['rundown','tapout','carnival','inside','underground']
+word=random.choice(word_list)
+
 while True:
     guess=input('Please enter a single letter:\n')
     if len(guess)!=1:
@@ -7,3 +12,11 @@ while True:
     else:
         print('You have guessed %s\n'%(guess))
         break
+if guess in word:
+    count=0
+    for x in word:
+        if x==guess:
+            count+=1
+    print('%s occurs %s times in the word'%(guess,count))
+else:
+    print('Sorry %s is not in the word'%(guess))
