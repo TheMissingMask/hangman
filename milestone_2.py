4653301ce10933ecf44156b338e3c96701e02d98
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-17 20:41:17 +0000

    Update milestone_2.py
    Trying to get the next one update but the validation is taking ages so who knowse

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,12 @@ word_list=[x.lower() for x in word_list]
 word=random.choice(word_list)
 
 guess=input('Please enter a letter:\n')
+
+if len(guess)!=1:
+  print('Oops!  That is not a valid input!')
+  guess=input('Please try again\n')
+elif len.isalpha()==False:
+  print('Oops!  That is not a valid input!')
+  guess=input('Please try again\n')
+else:
+  print('Good guess!')
