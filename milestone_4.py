e65952f9f4814b2f4cb0406af9eecb8719bfc41a
Ellis Jefferys <39962129+TheMissingMask@users.noreply.github.com>
2023-03-18 11:10:29 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,53 +1,51 @@
-import random
+mport random
 
 class Hangman:
+
     def __init__(self,word_list,num_lives=5):
+
         self.word_list=word_list
         self.num_lives=num_lives
+
         self.word=random.choice(word_list)
         self.num_letters=len(set(self.word))
+
         self.word_guessed=['_']
         self.list_of_guesses=[]
 
-    
+    def check_guess(self,guess):
 
-def check_guess(guess,word,hidden):
-    guess=guess.lower()
-    word=word.lower()
-    if guess in word:
-        count,i=0,0
-        for x in word:
-            if x==guess:
-                count+=1
-                hidden[i]=guess
-            i+=1
-        print('%s occurs %s times in the word'%(guess,count))
-        return hidden
-    else:
-        print('Sorry %s is not in the word'%(guess))
-        return False
-    
-def ask_for_input(word,hidden):
-    
-    while True:
-        guess=input('Please enter a single letter:\n')
-        if len(guess)!=1:
-            guess=input('Invalid letter.  Please, enter a single alphabetical character.')
-        elif guess.isalpha()==False:
-            guess=input('Invalid letter.  Please, enter a single alphabetical character.')
-        else:
-            print('You have guessed %s\n'%(guess))
-            break
-    hidden=check_guess(guess,word,hidden)
+        self.guess=guess
+        self.guess=self.guess.lower()
 
-def printHidden(hidden):
-    outString=''
-    for i in hidden:
-        outString+=i
-    print(outString)
-    
+        if self.guess in self.word:
+            count,i=0,0
+            for x in self.word:
+                if x==self.guess:
+                    count+=1
+                    self.word_guessed[i]=self.guess
+                i+=1
+            print('Good guess! %s is in the word'%(self.guess))
+#            print('%s occurs %s times in the word'%(guess,count))
+#            return self.word_guessed
+#        else:
+#            print('Sorry %s is not in the word'%(guess))
+            return False
+
+    def ask_for_input(self):
+
+        while True:
+            self.guess=input('Please enter a single letter:\n')
+            if len(self.guess)!=1:
+                self.guess=input('Invalid letter.  Please, enter a single alphabetical character.')
+            elif self.guess.isalpha()==False:
+                self.guess=input('Invalid letter.  Please, enter a single alphabetical character.')
+            elif self.guess in self.list_of_guesses:
+                print('You already tried that letter')
+            else:
+                print('You have guessed %s\n'%(self.guess))
+                self.list_of_guesses.append(self.guess)
+                break
 word_list=['rundown','tapout','carnival','inside','underground']
-word=random.choice(word_list)
-hidden=['_']*len(word)
-
-ask_for_input(word,hidden)
+hangman=Hangman(word_list)
+hangman.ask_for_input()
